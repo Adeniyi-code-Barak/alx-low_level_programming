212e3facbc4731df243b9a5cbcebb79e91eecfe1
Adeniyi-code-Barak <adeniyiogundiran9@gmail.com>
2023-06-22 15:17:59 -0700

    Funtions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,21 @@
 #include "main.h"
 
 /**
- * more_numbers - to print numbers 0 to 14
- * Description: to print numbers 0 to 14 on 10 lines
+ * more_numbers - print more numbers 
  */
 
 void more_numbers(void)
 {
-	int i = 0;
-	int j;
+	int i, j;
 
-	while (i < 10)
+	for (i = 1; i <=10; i++)
 	{
-		j = 0;
-
-		while (j <= 14)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
-			j++;
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
-		i++;
 	}
 }

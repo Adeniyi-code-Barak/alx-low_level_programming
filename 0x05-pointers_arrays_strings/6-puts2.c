fed0e672572942b50ac70406eb046656a023a27a
Adeniyi-code-Barak <adeniyiogundiran9@gmail.com>
2023-06-26 15:45:47 -0700

    Arrays

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,31 +1,20 @@
-#include "main.h"
-#include <stdio.h>
+#include"main.h"
 
 /**
- * puts2 - display other character of string,
- * followed by new line
- * @str: pointer to the string
- * Return: Return void
+ * puts2 - prints every character of a string
+ *
+ * @str: string parameter input
+ *
+ * Return: Noting
  */
 void puts2(char *str)
 {
-	int size = 0;
-	int i = 0;
-	int m;
+	int i;
 
-	for (size = 0; str[size] != '\0'; size++)
+	for (i = 0; str[i] != '\0'; ++i)
 	{
-		i++;
-	}
-	
-	i = size - 1;
-
-	for (m = 0; m <= 1; m++)
-	{
-		if (m % 2 == 0)
-		{
-			_putchar(str[m]);
-		}
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }

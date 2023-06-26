74ba007e927db22c0bf05f115be663a01f200eb9
Adeniyi-code-Barak <adeniyiogundiran9@gmail.com>
2023-06-26 10:04:52 -0700

    Nested

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphatbet - print all alphatbet in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter ; 'z' letter++);
+		_putchar(letter);
+	_putchar('\n');
+}
